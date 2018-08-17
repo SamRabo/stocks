38bd5590207afea5d03c9ee1676559127a31d903
JingS <Sam.Jing@rabobank.nl>
2018-08-17 16:38:47 +0200

    Chapter 2 Stock Card in grid-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { StocksService } from '.services/stocks.service';
+import { StocksService } from './services/stocks.service';
 import { SummaryComponent } from './components/summary/summary.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SummaryComponent
+    SummaryComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
