0617917272be372bbce9aad8f2b335f8c06b4c3c
JingS <Sam.Jing@rabobank.nl>
2018-08-17 21:34:49 +0200

    Chapter 2 Added Manage Stock form + routing paths

--- src/app/services/stocks.service.ts
+++ src/app/services/stocks.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-let stocks: Array<string> = ['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR'];
+let stocks: Array<string> = ['BTC-USD', 'ETH-USD', 'DASH-USD', 'NEO-USD', 'ETC-USD', 'ZEC-USD', 'LSK-USD', 'IOT-USD'];
 let service: string = 'https://angular2-in-action-api.herokuapp.com';
 
 export interface StockInterface {
